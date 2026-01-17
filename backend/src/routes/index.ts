dfa6c988e3bb19389c4429f5829f9f06eb68e910
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-01-17 21:16:25 +0530

    user router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,8 @@
 import {Router} from 'express'
+import { userRouter } from './user.js';
 const router = Router();
 router.get('/', (req, res)=>{
     res.send("Hello World");
 })
+router.use("/user", userRouter);
 export default router;
