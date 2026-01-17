8469be607f0bed16224907523864076de7fcc11b
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-01-17 21:20:49 +0530

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import router from "./routes/index.js";
 import dbConnect from "./config/db.js";
+import cors from "cors"
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 await dbConnect();
