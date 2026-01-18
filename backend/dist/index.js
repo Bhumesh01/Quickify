3e0db0a9654d7df5d62b60bb52a9542800f1dae5
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-01-18 17:24:49 +0530

    Signup EndPoint

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import router from "./routes/index.js";
 import dbConnect from "./config/db.js";
+import cors from "cors";
 const app = express();
+app.use(cors());
 app.use(express.json());
 await dbConnect();
 app.use("/api/v1", router);
---
+++ backend/dist/routes/user.js
@@ -0,0 +1,74 @@
+import { Router } from "express";
+import bcrypt from "bcrypt";
+import { User } from "../models/db.js";
+import z from "zod";
+export const userRouter = Router();
+const userZodSchema = z.object({
+    username: z.string().min(3, { message: "username must be atleast 3 characters long" }).max(30, { message: "username can't be greater than 30 characters" }),
+    email: z.email({ message: "please enter a valid email" }),
+    password: z.string().min(8, { message: "password must be atleast 8 characters long" }).max(20, { message: "password can't be greater than 20 characters" }).refine(val => /[A-Z]/.test(val), { message: "Password must contain at least one uppercase letter" }).refine(val => /[a-z]/.test(val), { message: "Password must contain at least one lowercase letter" }).refine(val => /[^A-Za-z0-9]/.test(val), { message: "Password must contain at least one special symbol" }).refine(val => /[0-9]/.test(val), { message: "Password must contain at least one digit" }),
+    confirmedPassword: z.string().min(8, { message: "Confirmed Password must be atleast 8 characters long" }).max(20, { message: "Confirmed Password can't be greater than 20 characters" }).refine(val => /[A-Z]/.test(val), { message: "Confirmed Password must contain at least one uppercase letter" }).refine(val => /[a-z]/.test(val), { message: "Confirmed Password must contain at least one lowercase letter" }).refine(val => /[^A-Za-z0-9]/.test(val), { message: "Confirmed Password must contain at least one special symbol" }).refine(val => /[0-9]/.test(val), { message: "Confirmed Password must contain at least one digit" }),
+    firstName: z.string(),
+    lastName: z.string()
+}).superRefine((data, ctx) => {
+    if (data.confirmedPassword != data.password) {
+        ctx.addIssue({
+            path: ["confirmedPassword"],
+            message: "Confirmed password does not match",
+            code: z.ZodIssueCode.custom
+        });
+    }
+});
+//SignUp Endpoint
+userRouter.post("/signup", async (req, res) => {
+    try {
+        const { success, data, error } = userZodSchema.safeParse(req.body);
+        if (!success) {
+            const formatted = error.flatten();
+            const usernameErrors = formatted.fieldErrors.username;
+            const passwordErrors = formatted.fieldErrors.password;
+            const emailErrors = formatted.fieldErrors.email;
+            const firstNameErrors = formatted.fieldErrors.firstName;
+            const lastNameErrors = formatted.fieldErrors.lastName;
+            const confirmedPasswordErrors = formatted.fieldErrors.confirmedPassword;
+            return res.status(400).json({
+                usernameErrors,
+                passwordErrors,
+                confirmedPasswordErrors,
+                emailErrors,
+                firstNameErrors,
+                lastNameErrors
+            });
+        }
+        const credentials = data;
+        const hash = await bcrypt.hash(credentials.password, 5);
+        const username = credentials.username;
+        const password = hash;
+        const email = credentials.email;
+        const firstName = credentials.firstName;
+        const lastName = credentials.lastName;
+        const exists = await User.findOne({
+            $or: [
+                { username: username },
+                { email: email }
+            ]
+        });
+        if (exists) {
+            return res.status(409).json({ message: "User already exists" });
+        }
+        await User.create({
+            username: username,
+            password: password,
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+        });
+        res.status(200).json({
+            message: "Successfully signed up"
+        });
+    }
+    catch (err) {
+        return res.status(500).json({ message: err });
+    }
+});
+//# sourceMappingURL=user.js.map
