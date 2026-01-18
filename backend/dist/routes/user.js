d4a2d405460429dd5a8482e8814ec59e4062e746
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-01-18 17:56:59 +0530

    signin endpoint

--- backend/dist/routes/user.js
+++ backend/dist/routes/user.js
@@ -2,15 +2,19 @@ import { Router } from "express";
 import bcrypt from "bcrypt";
 import { User } from "../models/db.js";
 import z from "zod";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 export const userRouter = Router();
-const userZodSchema = z.object({
+const baseZodSchema = z.object({
     username: z.string().min(3, { message: "username must be atleast 3 characters long" }).max(30, { message: "username can't be greater than 30 characters" }),
     email: z.email({ message: "please enter a valid email" }),
     password: z.string().min(8, { message: "password must be atleast 8 characters long" }).max(20, { message: "password can't be greater than 20 characters" }).refine(val => /[A-Z]/.test(val), { message: "Password must contain at least one uppercase letter" }).refine(val => /[a-z]/.test(val), { message: "Password must contain at least one lowercase letter" }).refine(val => /[^A-Za-z0-9]/.test(val), { message: "Password must contain at least one special symbol" }).refine(val => /[0-9]/.test(val), { message: "Password must contain at least one digit" }),
     confirmedPassword: z.string().min(8, { message: "Confirmed Password must be atleast 8 characters long" }).max(20, { message: "Confirmed Password can't be greater than 20 characters" }).refine(val => /[A-Z]/.test(val), { message: "Confirmed Password must contain at least one uppercase letter" }).refine(val => /[a-z]/.test(val), { message: "Confirmed Password must contain at least one lowercase letter" }).refine(val => /[^A-Za-z0-9]/.test(val), { message: "Confirmed Password must contain at least one special symbol" }).refine(val => /[0-9]/.test(val), { message: "Confirmed Password must contain at least one digit" }),
     firstName: z.string(),
     lastName: z.string()
-}).superRefine((data, ctx) => {
+});
+const signupSchema = baseZodSchema.superRefine((data, ctx) => {
     if (data.confirmedPassword != data.password) {
         ctx.addIssue({
             path: ["confirmedPassword"],
@@ -19,10 +23,11 @@ const userZodSchema = z.object({
         });
     }
 });
+const signinSchema = baseZodSchema.omit({ confirmedPassword: true, lastName: true, firstName: true });
 //SignUp Endpoint
 userRouter.post("/signup", async (req, res) => {
     try {
-        const { success, data, error } = userZodSchema.safeParse(req.body);
+        const { success, data, error } = signupSchema.safeParse(req.body);
         if (!success) {
             const formatted = error.flatten();
             const usernameErrors = formatted.fieldErrors.username;
@@ -69,6 +74,39 @@ userRouter.post("/signup", async (req, res) => {
     }
     catch (err) {
         return res.status(500).json({ message: err });
+    }
+});
+//SignIn Endpoint
+userRouter.post("/signin", async (req, res) => {
+    try {
+        const { success, data, error } = signinSchema.safeParse(req.body);
+        if (!success) {
+            const formatted = error.flatten().fieldErrors;
+            return res.status(400).json(formatted);
+        }
+        const user = await User.findOne({ $or: [{ username: data.username }, { email: data.email }] });
+        if (!user) {
+            return res.status(404).json({
+                message: "User does not exists",
+            });
+        }
+        const result = await bcrypt.compare(data.password, user.password);
+        if (!result) {
+            return res.status(404).json({
+                message: "Incorrect Password",
+            });
+        }
+        const token = jwt.sign({ userId: user._id }, `${process.env.SECRET}`);
+        return res.status(200).json({
+            message: "Successfully Signed In",
+            token: token
+        });
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(411).json({
+            "message": "Error while Logging In"
+        });
     }
 });
 //# sourceMappingURL=user.js.map
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,17 +3,21 @@ import type { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import { User } from "../models/db.js";
 import z from "zod";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const userRouter = Router();
 
-const userZodSchema = z.object({
+const baseZodSchema = z.object({
     username: z.string().min(3, {message: "username must be atleast 3 characters long"}).max(30, {message: "username can't be greater than 30 characters"}),
     email: z.email({message: "please enter a valid email"}),
     password: z.string().min(8, {message: "password must be atleast 8 characters long"}).max(20, {message: "password can't be greater than 20 characters"}).refine(val=>/[A-Z]/.test(val), {message: "Password must contain at least one uppercase letter"}).refine(val => /[a-z]/.test(val), { message: "Password must contain at least one lowercase letter" }).refine(val => /[^A-Za-z0-9]/.test(val), { message: "Password must contain at least one special symbol" }).refine(val => /[0-9]/.test(val), { message: "Password must contain at least one digit" }),
     confirmedPassword: z.string().min(8, {message: "Confirmed Password must be atleast 8 characters long"}).max(20, {message: "Confirmed Password can't be greater than 20 characters"}).refine(val=>/[A-Z]/.test(val), {message: "Confirmed Password must contain at least one uppercase letter"}).refine(val => /[a-z]/.test(val), { message: "Confirmed Password must contain at least one lowercase letter" }).refine(val => /[^A-Za-z0-9]/.test(val), { message: "Confirmed Password must contain at least one special symbol" }).refine(val => /[0-9]/.test(val), { message: "Confirmed Password must contain at least one digit" }),
     firstName: z.string(),
     lastName: z.string()
-}).superRefine((data, ctx)=>{
+});
+const signupSchema = baseZodSchema.superRefine((data, ctx)=>{
     if(data.confirmedPassword!=data.password){
         ctx.addIssue({
           path: ["confirmedPassword"],
@@ -21,12 +25,13 @@ const userZodSchema = z.object({
           code: z.ZodIssueCode.custom
         });
     }
-})
+});
+const signinSchema = baseZodSchema.omit({confirmedPassword: true, lastName: true, firstName: true});
 
 //SignUp Endpoint
 userRouter.post("/signup", async (req:Request, res:Response)=>{
     try{
-        const {success, data, error} = userZodSchema.safeParse(req.body);
+        const {success, data, error} = signupSchema.safeParse(req.body);
         if(!success){
             const formatted = error.flatten();
             const usernameErrors = formatted.fieldErrors.username;
@@ -44,7 +49,7 @@ userRouter.post("/signup", async (req:Request, res:Response)=>{
               lastNameErrors
             });
         }
-        const credentials:z.infer<typeof userZodSchema> = data;
+        const credentials:z.infer<typeof signupSchema> = data;
         const hash = await bcrypt.hash(credentials.password, 5);
         const username = credentials.username;
         const password = hash;
@@ -73,5 +78,39 @@ userRouter.post("/signup", async (req:Request, res:Response)=>{
     }
     catch(err:any){
         return res.status(500).json({ message: err });
+    }
+})
+
+//SignIn Endpoint
+userRouter.post("/signin", async(req:Request, res:Response)=>{
+    try{
+        const {success, data, error} = signinSchema.safeParse(req.body);
+        if(!success){
+            const formatted = error.flatten().fieldErrors;
+            return res.status(400).json(formatted);
+        }
+        const user = await User.findOne({$or: [{username: data.username}, {email: data.email}]});
+        if(!user){
+            return res.status(404).json({
+                message: "User does not exists",
+            });
+        }
+        const result = await bcrypt.compare(data.password, user.password);
+        if(!result){
+            return res.status(404).json({
+                message: "Incorrect Password",
+            });
+        }
+        const token = jwt.sign({userId: user._id}, `${process.env.SECRET}`);
+        return res.status(200).json({
+            message: "Successfully Signed In",
+            token : token
+        })
+    }
+    catch(err:any){
+        console.log(err);
+        return res.status(411).json({
+            "message": "Error while Logging In"
+        });
     }
 })
