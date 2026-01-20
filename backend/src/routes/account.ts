975be3e9bd604e4cc8eb9c21258b1a6f833f2cd4
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-01-20 20:17:06 +0530

    Added transaction Logic

--- backend/src/routes/account.ts
+++ backend/src/routes/account.ts
@@ -5,13 +5,18 @@ import mongoose from "mongoose";
 import { Account } from "../models/db.js";
 export const accountRouter = Router();
 
+interface TransferTypes{
+    to: string,
+    amount: number
+}
+
 accountRouter.use(authMiddleware);
 
-interface customRequest extends Request{
+interface CustomRequest extends Request{
     userId?: string
 }
 
-accountRouter.get("/balance", async(req: customRequest, res: Response)=>{
+accountRouter.get("/balance", async(req: CustomRequest, res: Response)=>{
     try{
         if(!req.userId){
             return res.status(400).json({
@@ -21,17 +26,68 @@ accountRouter.get("/balance", async(req: customRequest, res: Response)=>{
         const userId = new mongoose.Types.ObjectId(req.userId);
         const response = await Account.findOne({
             userId: userId
-        }).populate('userId');
+        });
         if(!response){
             return res.status(400).json({
                 message: "User doesn't exists"
             })
         }
+        return res.json({
+            message: "Successfully Fetched Balance",
+            balance: response.balance
+        })
     }
     catch(err){
         console.log(err);
-        return res.status(400).send({
+        return res.status(500).send({
             message: "An Error Occurred! Please try again later"
+        })
+    }
+})
+
+accountRouter.post("/transfer", async (req:CustomRequest, res: Response)=>{
+    try{
+        if(!req.userId){
+            return res.status(400).json({
+                message: "Invalid Request"
+            });
+        }
+        const senderId = new mongoose.Types.ObjectId(req.userId);
+        const data:TransferTypes = req.body;
+        if(!data.to || !data.amount){
+            return res.status(400).json({
+                message: "Please provide the recepient and the amount"
+            })
+        }
+        const receiverId = new mongoose.Types.ObjectId(data.to);
+
+        const sender = await Account.findOne({userId: senderId});
+        const receiver = await Account.findOne({userId: receiverId});
+        if(!sender || !receiver){
+            return res.status(400).json({
+                message: "Invalid Account"
+            })
+        }
+        if(sender.balance < data.amount){
+            return res.status(400).json({
+                message: "Insufficient Balance"
+            })
+        }
+        const debit = await Account.findByIdAndUpdate(senderId, {$inc: {balance: sender.balance - data.amount}});
+        const credit = await Account.findByIdAndUpdate(receiverId, {$inc: {balance: receiver.balance+data.amount}});
+        if(!debit || !credit){
+            return res.status(400).json({
+                message: "Transaction failed"
+            });
+        }
+        return res.status(200).json({
+            message: "Transaction Successful"
+        });
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json({
+            message: "An Error Occurred! Transaction Failed!"
         })
     }
 })
